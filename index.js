edbca1c052d8051f3cef02bc28a0cd0590775206
Yurou <yrq1905@gmail.com>
2025-11-17 08:58:11 +1030

    Add scoreboard project

---
+++ index.js
@@ -0,0 +1,29 @@
+function homeAddOne() {
+    sum = document.getElementById("display_home");
+    sum.innerText = Number(sum.innerText) + 1;
+}
+
+function homeAddTwo() {
+  sum = document.getElementById("display_home");
+  sum.innerText = Number(sum.innerText) + 2;
+}
+
+function homeAddThree() {
+  sum = document.getElementById("display_home");
+  sum.innerText = Number(sum.innerText) + 3;
+}
+
+function guestAddOne() {
+  sum = document.getElementById("display_guest");
+  sum.innerText = Number(sum.innerText) + 1;
+}
+
+function guestAddTwo() {
+  sum = document.getElementById("display_guest");
+  sum.innerText = Number(sum.innerText) + 2;
+}
+
+function guestAddThree() {
+  sum = document.getElementById("display_guest");
+  sum.innerText = Number(sum.innerText) + 3;
+}
